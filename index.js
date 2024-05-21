80349f60e6d665db0aa8843774b43e6d40b5c56b
H.K Sahan Tharaka <161915749+HKSahanTharaka@users.noreply.github.com>
2024-05-21 10:39:34 +0530

    modified:   index.js 	modified:   node_modules/.package-lock.json 	new file:   node_modules/cors/CONTRIBUTING.md 	new file:   node_modules/cors/HISTORY.md 	new file:   node_modules/cors/LICENSE 	new file:   node_modules/cors/README.md 	new file:   node_modules/cors/lib/index.js 	new file:   node_modules/cors/package.json 	new file:   node_modules/object-assign/index.js 	new file:   node_modules/object-assign/license 	new file:   node_modules/object-assign/package.json 	new file:   node_modules/object-assign/readme.md 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5000;
 
 const Project = require('./Project');
 const Blog = require('./Blog');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
